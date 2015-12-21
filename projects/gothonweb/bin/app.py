a56d10683fae8d0b6a82db972ccc274fce33ac8d
rythmE <poiuwertion@gmail.com>
2015-12-21 23:30:21 +0800

    gothonweb need hello_form.html

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,16 +1,20 @@
 import web
 
 urls = (
-	'/', 'Index'
+	'/hello', 'Index'
 )
 
 app = web.application(urls, globals())
 
 render = web.template.render('templates/')
 
-class Index:
+class Index():
 	def GET(self):
-		greeting = "Hello World"
+		return render.hello_form()
+		
+	def POST(self):
+		form = web.input(name="Nobody", greet="Hello")
+		greeting = "%s, %s" % (form.greet, form.name)	
 		return render.index(greeting = greeting)
 		
 if __name__ == "__main__":
